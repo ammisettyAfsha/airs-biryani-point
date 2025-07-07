d0ac524b1dc4ac6a4940c086b2ba509f39ee6c95
AIRS <ammisetty.afsha@gmail.com>
2025-07-07 12:00:50 -0300

    created HeroBanner for Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroBanner from "@/components/HeroBanner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>AIRS Biryani point</h1>
+      <HeroBanner/>
     </div>
   );
 }
