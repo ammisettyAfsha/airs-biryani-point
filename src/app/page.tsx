f11b61881acca3889426df273ac0498a64995cf2
AIRS <ammisetty.afsha@gmail.com>
2025-07-08 15:36:09 -0300

    fixed production bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-
-
-
 import HeroBanner from "../components/HeroBanner";
 import MenuPreview from "../components/MenuPreview";
 import Footer from "../components/Footer";
-
 
 export default function Home() {
   return (
